dda81633d6c5d4b684149ae54b199a5e0edb84bd
Kamil <54462478+Kejson@users.noreply.github.com>
2020-01-31 01:33:36 +0100

    Update back.php

--- back.php
+++ back.php
@@ -1,6 +1,6 @@
 <?php
 
- $connect = mysqli_connect('localhost', 'root', '$nazwaBazy')or die("Error");
+ $connect = mysqli_connect('localhost', 'root', 'blog_testowy')or die("Error");
  //$nazwaBazy jeszzce nie ma
 
 ?>
