9ab9ea9e7853ffdd4161d8619bd521592aa3c9a9
Sebastian Siarczyński <sebastiansiarczynski@protonmail.com>
2020-02-17 20:14:55 +0100

    Commit
    Zrobione posty jedyne co to z responsywnoscia na Ipada Pro musze powalczyc ale chuj nie wiem na razie jak to zrobic najwyzej tylko tego nie bedzie jak jakies zazalenia to piszczie na grupie

--- frontEnd/src/app/Component/List.js
+++ frontEnd/src/app/Component/List.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
-const List = ( {title,desc,author} ) => {
+const List = ( {title,desc,author,img,src} ) => {
     
     return(
         <li key={title}>
+            <section><img src={img} className='img-fluid img-thumbnail' alt='Obrazek trele morele'></img></section>
             <h1>{title}</h1>
             <p>{desc}</p>
-            <p>{author}</p>
+            <p>autor: <span>{author}</span></p>
+            <section><a href={src} className='btn btn-lg btn-primary'>Zobacz post!</a> <button className='btn btn-lg btn-danger'><i class="far fa-heart"></i></button></section>
         </li>
     )
 
