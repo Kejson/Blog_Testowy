b2928a0ee3124c3eb8533ed3578edbf1aa363488
Sebastian Siarczyński <sebastiansiarczynski@protonmail.com>
2020-02-19 17:14:24 +0100

    Commit
    Dla Jarusia

---
+++ frontEnd/build/precache-manifest.4ae18465dccd9d8e491f8036b457158a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae142c09fd835af4a5c950f06bc67b1f",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "857911bf942af5666a82",
+    "url": "/react/static/css/main.9ccc6094.chunk.css"
+  },
+  {
+    "revision": "c88cebed7dec8a545eeb",
+    "url": "/react/static/js/2.250dc4af.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react/static/js/2.250dc4af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "857911bf942af5666a82",
+    "url": "/react/static/js/main.c5f2314a.chunk.js"
+  },
+  {
+    "revision": "3872d251f8a3da2fc8e6",
+    "url": "/react/static/js/runtime-main.4e7c8bdf.js"
+  }
+]);
---
+++ frontEnd/public/js.js
@@ -0,0 +1,5 @@
+(function(){
+    document.getElementById('login').addEventListener('click', () => {
+        document.getElementsByClassName('loguj')[0].classList.toggle('logujActive') 
+    })
+})()
